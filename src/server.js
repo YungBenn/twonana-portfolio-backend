bf6c2bddfdae0214d4857dbd2a506b39fb283280
YungBenn <adisuryo22@gmail.com>
2023-03-19 12:39:14 +0700

    cleaning

--- src/server.js
+++ src/server.js
@@ -3,19 +3,25 @@ dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import session from 'express-session';
+import memory from 'memorystore';
 import { NftRouter } from './routes/nft.route.js';
 import { connect } from './utils/connect.js';
 import { apiLimiter } from './middlewares/ratelimit.js';
 import { adminRouter } from './routes/admin.route.js';
 
 const app = express();
+const MemoryStore = memory(session);
 const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.set('trust proxy', 1);
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
+    store: new MemoryStore({
+      checkPeriod: 86400000,
+    }),
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -24,7 +30,6 @@ app.use(
   }),
 );
 app.use(cors());
-app.set('trust proxy', 1);
 
 // Route
 app.use('/api/nft', apiLimiter, NftRouter);
